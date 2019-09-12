fd1c0931a3defb9df202e6257de96be5e73a7ed4
kisekitw <kisekitw@gmail.com>
2019-09-12 14:53:41 +0800

    setup app for heroku

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
 });
